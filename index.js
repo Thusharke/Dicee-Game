077bf10c1682c8cf38fdb371e38bad9ce49ee22d
thusharke <thushar.ke25@gmail.com>
2020-10-19 22:43:37 +0530

    Add Play-Again Button

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+function result(){
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+  var randomImageSource1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+  document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Play 1 Wins!";
-}
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins!";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
+  if (randomNumber1 > randomNumber2) {                           //If player 1 wins
+    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+  } else if (randomNumber2 > randomNumber1) {                    //If player 2 wins
+    document.querySelector("h1").innerHTML = "Player 2 Wins!";
+  }else {                                                        //If it is a draw
+    document.querySelector("h1").innerHTML = "Draw!";
+  }
 }
